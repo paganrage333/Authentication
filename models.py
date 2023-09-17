cb3f4c3b271c166561c4296f57fdbc56edcdc2e2
paganrage333 <aragan@unca.edu>
2023-09-17 16:08:14 -0400

    html pages

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
 
 class User(db.Model):
 
@@ -21,8 +26,45 @@ class User(db.Model):
                            nullable=False)
     last_name = db.Column(db.String(30), 
                           nullable=False)
+    
+    feedback = db.relationship("Feedback", backref="user")
 
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
 
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)
+        return user
+    
+    @classmethod
+    def authenticate(cls, username, password):
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
+        
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
